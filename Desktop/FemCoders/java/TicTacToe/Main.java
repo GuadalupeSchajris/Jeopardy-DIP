f7c680b921c5eb41bdaa59db7961a82990aff26a
GuadalupeSchajris <haniguadalupe@gmail.com>
2025-03-07 10:44:20 +0100

    pair 1

---
+++ Desktop/FemCoders/java/TicTacToe/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] tablero = {"","","","","","","","","",""};
+        showTablero(tablero);
+    
+    }
+
+   public static void showTablero(String [] tablero) {
+    System.out.println(tablero[0] + " | "+ tablero[1]+" | " + tablero[2]);
+    System.out.println(tablero[3] + " | "+ tablero[4]+" | " + tablero[5]);
+    System.out.println(tablero[6] + " | "+ tablero[7]+" | " + tablero[8]);
+   }
+}
